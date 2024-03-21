32f3e1af0279ed99ecc5d899ba7035b1d426fe46
Matteo Cavestri <matteo.cavestri@cavestri.net>
2024-03-21 13:54:50 +0100

    Add: Nvdash startup

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,6 +1,22 @@
 ---@type ChadrcConfig
 local M = {}
 
+local headers = require("configs.headers")
+
+local function get_header()
+  if vim.g.random_header then
+    local headerNames = {}
+    for name, _ in pairs(headers) do
+      table.insert(headerNames, name)
+    end
+    local randomName = headerNames[math.random(#headerNames)]
+    local randomHeader = headers[randomName]
+    return randomHeader
+  else
+    return headers["nvchad"]
+  end
+end
+
 M.ui = {
   theme = "catppuccin",
   statusline = {
@@ -16,6 +32,20 @@ M.ui = {
   hl_override = {
     Comment = { italic = true },
     ["@comment"] = { italic = true },
+  },
+  nvdash = {
+    load_on_startup = true,
+    header = get_header(),
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰔚  Find All", "Spc f a", "Telescope find_all" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      -- { "  Find Projects", "Spc f p", "Telescope projects" },
+      -- { "󰔚  Find Sessions", "Spc f s", "Telescope persisted" },
+      -- { "  Themes", "Spc f t", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
   },
 }
 
