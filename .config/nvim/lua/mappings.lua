3bb103038c70b0172725f791245e85e50969146b
Matteo Cavestri <matteo.cavestri@cavestri.net>
2024-03-21 21:35:12 +0100

    Add: Dap mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,68 +2,61 @@ require("nvchad.mappings")
 
 -- add yours here
 
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
 -- General key mappings
-map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "window left" })
-map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "window right" })
-map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "window down" })
-map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "window up" })
 
--- DAP (Debug Adapter Protocol) key mappings
-map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Add breakpoint at line" })
-map("n", "<leader>ds", "<cmd> DapContinue <CR>", { desc = "Run or continue the debugger" })
-map("n", "<leader>dus", function()
-  local widgets = require("dap.ui.widgets")
-  local sidebar = widgets.sidebar(widgets.scopes)
-  sidebar.open()
-end, { desc = "Open debugging sidebar" })
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Window Left" })
+map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Window Right" })
+map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Window Down" })
+map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Window Up" })
 
--- Crates key mappings
-map("n", "<leader>ru", function()
-  require("crates").upgrade_all_crates()
-end, { desc = "Update crates" })
+-- DAP
 
--- DAP Python key mappings
-map("n", "<leader>dps", function()
-  require("dap-python").test_method()
-end, { desc = "Run Python debugger" })
-
--- Rustacean Vim key mappings
-map("n", "<leader>drs", "<cmd> RustLsp debuggables <CR>", { desc = "Run Rust debuggables" })
-
--- DAP Go key mappings
-map("n", "<leader>dgt", function()
-  require("dap-go").debug_test()
-end, { desc = "Debug go test" })
-map("n", "<leader>dgl", function()
-  require("dap-go").debug_last()
-end, { desc = "Debug last go test" })
-
--- Gopher key mappings
-map("n", "<leader>gsj", "<cmd> GoTagAdd json <CR>", { desc = "Add json struct tags" })
-map("n", "<leader>gsy", "<cmd> GoTagAdd yaml <CR>", { desc = "Add yaml struct tags" })
+map("n", "<leader>dc", "<cmd>lua require'dap'.continue()<CR>", { desc = "Dap Continue", silent = true })
+map("n", "<leader>do", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Dap Step Over", silent = true })
+map("n", "<leader>di", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Dap Step Into", silent = true })
+map("n", "<leader>du", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Dap Step Out", silent = true })
+map(
+	"n",
+	"<leader>db",
+	"<cmd>lua require'dap'.toggle_breakpoint()<CR>",
+	{ desc = "Dap Toggle Breakpoint", silent = true }
+)
+map(
+	"n",
+	"<leader>dB",
+	"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+	{ desc = "Breakpoint Condition", silent = true }
+)
+map("n", "<leader>dd", "<cmd>lua require'dapui'.toggle()<CR>", { desc = "Dap UI", silent = true })
+map("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Dap Run Last", silent = true })
 
 -- ChatGPT key mappings
+
 map("n", "<leader>cg", "<cmd> ChatGPT <CR>", { desc = "ChatGPT" })
 
 -- Todo Comments mappings
+
 map("n", "<leader>fT", "<cmd>TodoTelescope<CR>", { desc = "Find Todos" })
 
 -- Markdown Preview
 map("n", "<leader>m", function()
-  if vim.bo.filetype == "markdown" then
-    vim.cmd("MarkdownPreviewToggle")
-  else
-    vim.notify("Only available in markdown", vim.log.levels.WARN, { title = "Markdown-Preview" })
-  end
+	if vim.bo.filetype == "markdown" then
+		vim.cmd("MarkdownPreviewToggle")
+	else
+		vim.notify("Only available in markdown", vim.log.levels.WARN, { title = "Markdown-Preview" })
+	end
 end, { desc = "Markdown Preview" })
 
 -- Compiler
 
-map("n", "<leader>cp", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+map("n", "<leader>rr", "<cmd>CompilerRedo<CR>", { desc = "Compiler Redo Last Action", silent = true })
+map("n", "<leader>ro", "<cmd>CompilerOpen<CR>", { desc = "Compiler Open", silent = true })
+map("n", "<leader>rs", "<cmd>CompilerStop<CR>", { desc = "Compiler Stop All Tasks", silent = true })
+map("n", "<leader>rt", "<cmd>CompilerToggleResults<CR>", { desc = "Compiler Toggle Results", silent = true })
