b40c6c2a3f287079a207748e04829730577b56b6
Matteo Cavestri <matteo.cavestri@cavestri.net>
2024-03-31 12:32:41 +0200

    Add: Sessionx-tmux, Mod: Tmux UI, Nvim Status

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,52 +1,52 @@
 ---@type ChadrcConfig
 local M = {}
 
-local headers = require("configs.headers")
+local headers = require "configs.headers"
 
 local function get_header()
-	if vim.g.random_header then
-		local headerNames = {}
-		for name, _ in pairs(headers) do
-			table.insert(headerNames, name)
-		end
-		local randomName = headerNames[math.random(#headerNames)]
-		local randomHeader = headers[randomName]
-		return randomHeader
-	else
-		return headers["pacman"]
-	end
+  if vim.g.random_header then
+    local headerNames = {}
+    for name, _ in pairs(headers) do
+      table.insert(headerNames, name)
+    end
+    local randomName = headerNames[math.random(#headerNames)]
+    local randomHeader = headers[randomName]
+    return randomHeader
+  else
+    return headers["pacman"]
+  end
 end
 
 M.ui = {
-	theme = "catppuccin",
-	statusline = {
-		theme = "minimal", -- default/vscode/vscode_colored/minimal
-		separator_style = "block", -- default/round/block/arrow separators only for "default"; (round and block for the minimal only)
-		overriden_modules = nil,
-		--  theme_toggle = { "catppuccin", "yoru" },
-	},
-	tabufline = {
-		lazyload = true,
-		overriden_modules = nil,
-	},
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
-	nvdash = {
-		load_on_startup = true,
-		header = get_header(),
-		buttons = {
-			{ "  Find File", "Spc f f", "Telescope find_files" },
-			{ "󰔚  Find All", "Spc f a", "Telescope find_all" },
-			{ "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-			{ "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-			-- { "  Find Projects", "Spc f p", "Telescope projects" },
-			-- { "󰔚  Find Sessions", "Spc f s", "Telescope persisted" },
-			-- { "  Themes", "Spc f t", "Telescope themes" },
-			{ "  Mappings", "Spc c h", "NvCheatsheet" },
-		},
-	},
+  theme = "catppuccin",
+  statusline = {
+    theme = "minimal", -- default/vscode/vscode_colored/minimal
+    separator_style = "round", -- default/round/block/arrow separators only for "default"; (round and block for the minimal only)
+    overriden_modules = nil,
+    --  theme_toggle = { "catppuccin", "yoru" },
+  },
+  tabufline = {
+    lazyload = true,
+    overriden_modules = nil,
+  },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+  nvdash = {
+    load_on_startup = true,
+    header = get_header(),
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰔚  Find All", "Spc f a", "Telescope find_all" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      -- { "  Find Projects", "Spc f p", "Telescope projects" },
+      -- { "󰔚  Find Sessions", "Spc f s", "Telescope persisted" },
+      -- { "  Themes", "Spc f t", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
 }
 
 return M
